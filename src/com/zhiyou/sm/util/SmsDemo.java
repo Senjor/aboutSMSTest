02ff15b48475ab7f2d37c3d4a07eb4d623b4b538
Senjor <377844325@qq.com>
2020-08-13 14:33:05 +0800

    Update SmsDemo.java

--- src/com/zhiyou/sm/util/SmsDemo.java
+++ src/com/zhiyou/sm/util/SmsDemo.java
@@ -37,8 +37,8 @@ public class SmsDemo {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIjlwKyXmUpMyX";
-    static final String accessKeySecret = "Ys9kElwQIMmfFgXiFhuShuzPe0wNQs";
+    static final String accessKeyId = "xxxx";
+    static final String accessKeySecret = "xxxx";
 
     public static SendSmsResponse sendSms(String tel,String code) throws ClientException {
 
